0c7910ce2054c8a0a5c1445cf11ac90e7e3d1f74
LIYAD <LIYAD@LAPTOP-832FCV01>
2026-05-21 11:32:49 +0530

    3

---
+++ src/com/javaprograms/Test4.java
@@ -0,0 +1,12 @@
+package com.javaprograms;
+
+public class Test4 {
+         static int a=10;
+          static int b=20;
+	public static void main(String[] args) {
+		System.out.println(a+b);
+		
+
+	}
+
+}
